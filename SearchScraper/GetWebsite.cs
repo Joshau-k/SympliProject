db46019ac1dfb9e984f5fb1676951425a592794e
Josh <Josh@C44>
2021-01-31 21:17:32 +1100

    Created cache to prevent google blocking requests

--- SearchScraper/GetWebsite.cs
+++ SearchScraper/GetWebsite.cs
@@ -23,9 +23,13 @@ namespace SearchScraper
                 fullUrl += $"{name}={value}";
             }
 
-            //fullUrl = Uri.EscapeUriString(fullUrl);
             fullUrl = new UriBuilder(fullUrl).Uri.ToString();
-            // fullUrl = @"https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow"; 
+
+            WebCache cache = new WebCache(1000 * 60 * 60);
+            string cachedHtml = cache.GetEntry(fullUrl);
+            if (cachedHtml != null)
+                return cachedHtml;
+
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(fullUrl);
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
             string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
@@ -34,8 +38,12 @@ namespace SearchScraper
             using (Stream stream = response.GetResponseStream())
             using (StreamReader reader = new StreamReader(stream))
             {
-                return reader.ReadToEnd();
+                string html = reader.ReadToEnd();
+                cache.AddEntry(fullUrl, html);
+                return html;
             }
+
+            
         }
 
         
@@ -55,6 +63,44 @@ namespace SearchScraper.Test
             doc.LoadHtml(html);
 
             Assert.That(doc.ParseErrors, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        public void TestGetHtmlUsesCachedValue()
+        {
+            WebCache cache = new WebCache();
+            try
+            {
+                cache.Clear();
+
+                cache.AddEntry("http://google.com/search?q=e-settlements", "Test Cache Is Used");
+
+                var html = GetWebsite.GetHtml("google.com/search", ("q", "e-settlements"));
+
+                Assert.That(html, Is.EqualTo("Test Cache Is Used"));
+            }
+            finally
+            {
+                cache.Clear();
+            }
+        }
+
+        [Test]
+        public void TestGetHtmlSetsCachedValue()
+        {
+            WebCache cache = new WebCache();
+            try
+            {
+                cache.Clear();
+
+                var html = GetWebsite.GetHtml("google.com/search", ("q", "e-settlements"));
+
+                Assert.That(string.Equals(html, cache.GetEntry("http://google.com/search?q=e-settlements")), Is.EqualTo(true));
+            }
+            finally
+            {
+                cache.Clear();
+            }
         }
     }
 }
---
+++ SearchScraper/WebCache.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using NUnit;
+using NUnit.Framework;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace SearchScraper
+{
+    public class WebCache
+    {
+        string cacheFile = "cache.txt";
+        int timeoutmilliseconds = 1000 * 60 * 60;
+        public WebCache()
+        {
+
+        }
+
+        public WebCache(int milliseconds)
+        {
+            timeoutmilliseconds = milliseconds;
+        }
+
+        public void AddEntry(string fullurl, string html)
+        {
+            fullurl = System.Web.HttpUtility.UrlEncode(fullurl);
+            html = System.Web.HttpUtility.UrlEncode(html);
+            var newValue = (html, DateTime.Now);
+
+            Dictionary<string, (string, DateTime)> cache = GetCache();
+            if (cache.ContainsKey(fullurl))
+                cache[fullurl] = newValue;
+            else
+                cache.Add(fullurl, newValue);
+            File.WriteAllLines(cacheFile, cache.Select(x => $"{x.Key}|{x.Value.Item1}|{x.Value.Item2}"));
+        }
+
+        private Dictionary<string, (string value, DateTime time)> GetCache()
+        {
+            if (!File.Exists(cacheFile))
+                File.WriteAllText(cacheFile, "");
+
+            Dictionary<string, (string, DateTime)> dict = new Dictionary<string, (string, DateTime)>();
+            foreach (string line in File.ReadLines(cacheFile))
+            {
+                string key = line.Split("|")[0];
+                string value = line.Split("|")[1];
+                DateTime time = DateTime.Parse(line.Split("|")[2]);
+                dict.Add(key, (value, time));
+            }
+
+            return dict;
+        }
+
+        public string GetEntry(string key)
+        {
+            key = System.Web.HttpUtility.UrlEncode(key);
+            var dict = GetCache();
+            if (dict.ContainsKey(key) && dict[key].time.AddMilliseconds(timeoutmilliseconds) > DateTime.Now)
+                return System.Web.HttpUtility.UrlDecode(dict[key].value);
+            return null;
+        }
+
+        public void Clear()
+        {
+            File.Delete(cacheFile);
+        }
+        
+    }
+}
+
+namespace SearchScraper.Test
+{
+
+    public class WebCacheTest
+    {
+        [Test]
+        public void TestGetCache()
+        {
+            File.WriteAllText("cache.txt", $"key|value|{DateTime.Now.AddDays(1)}");
+
+            string html = new WebCache().GetEntry("key");
+            Assert.That(html, Is.EqualTo("value"));
+        }
+
+        //[Test]
+        //public void TestSetCache()
+        //{
+        //    new WebCache().AddEntry("key1", "html1");
+        //    var line1 = File.ReadLines("cache.txt").First();
+        //    Assert.That(line1, Is.EqualTo("key1=html1"));
+        //}
+
+        //[Test]
+        //public void TestSetCacheMultiple()
+        //{
+        //    new WebCache().AddEntry("key1", "html1");
+        //    new WebCache().AddEntry("key3", "html4");
+        //    var line1 = File.ReadLines("cache.txt").First();
+        //    var line2 = File.ReadLines("cache.txt").ToList()[1];
+        //    Assert.That(line1, Is.EqualTo("\"key1\"=\"html1\""));
+        //    Assert.That(line2, Is.EqualTo("\"key3\"=\"html4\""));
+        //}
+
+        [Test]
+        public void TestSetGetCache()
+        {
+            var cache = new WebCache();
+            cache.AddEntry("key1", "html1");
+            cache.AddEntry("key4", "html2");
+            cache.AddEntry("key5", "html6");
+            Assert.That(cache.GetEntry("key1"), Is.EqualTo("html1"));
+            Assert.That(cache.GetEntry("key4"), Is.EqualTo("html2"));
+            Assert.That(cache.GetEntry("key5"), Is.EqualTo("html6"));
+        }
+
+        [Test]
+        public void TestSetCacheTwice()
+        {
+            var cache = new WebCache();
+            cache.AddEntry("key1", "html1");
+            cache.AddEntry("key1", "html2");
+            Assert.That(cache.GetEntry("key1"), Is.EqualTo("html2"));
+        }
+
+        [Test]
+        public void TestCacheEntryMissing()
+        {
+            var cache = new WebCache();
+            cache.AddEntry("key1", "html1");
+            Assert.That(cache.GetEntry("key2"), Is.Null);
+        }
+
+        [Test]
+        public void TestCacheTimeout()
+        {
+            var timeoutMillis = 1000;
+            var cache = new WebCache(timeoutMillis);
+            cache.AddEntry("key1", "html1");
+            Assert.That(cache.GetEntry("key1"), Is.EqualTo("html1"));
+            Thread.Sleep(timeoutMillis + 100);
+            Assert.That(cache.GetEntry("key1"), Is.Null);
+        }
+
+        [Test]
+        public void TestCacheWorksWithUrlAndHtml()
+        {
+            var cache = new WebCache();
+            string url = "http://google.com/search?q=e-settlements";
+            string html = @"<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First | Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>";
+            cache.AddEntry(url, html);
+            Assert.That(cache.GetEntry(url), Is.EqualTo(html));
+        }
+
+        [Test]
+        public void TestCacheWorksWithNewLineAndCarriageReturn()
+        {
+            var cache = new WebCache();
+            string url = "http://google.com/search?q=e-settlements";
+            string html = String.Format(@"<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First | Heading</h1>
+<p>My first paragraph.</p>
+{0}
+{1}
+{2}
+</body>
+</html>", System.Environment.NewLine, "\r", "\n");
+            cache.AddEntry(url, html);
+            Assert.That(cache.GetEntry(url), Is.EqualTo(html));
+        }
+
+        [Test]
+        public void TestCacheWorksWithTestHtmlDoc()
+        {
+            TextReader tr = new StreamReader(@"TestSearchResult.txt");
+            string html = tr.ReadToEnd();
+
+            var cache = new WebCache();
+            string url = "http://google.com/search?q=e-settlements";
+            cache.AddEntry(url, html);
+            Assert.That(cache.GetEntry(url), Is.EqualTo(html));
+        }
+
+        //
+
+        [SetUp]
+        public void Setup()
+        {
+            var cache = new WebCache();
+            cache.Clear();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            var cache = new WebCache();
+            cache.Clear();
+        }
+    }
+}
